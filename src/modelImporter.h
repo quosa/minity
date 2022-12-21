e5f906f13c7e6ab4fe2aaf7799052f5bbc7bc133
Jussi Kuosa <jussiku@unity3d.com>
2022-12-22 01:58:44 +0200

    model loader support for normals and texture coordinates

--- src/modelImporter.h
+++ src/modelImporter.h
@@ -11,6 +11,7 @@
 namespace minity
 {
 
+// for texture coordinates (u, v)
 struct vec2
 {
     float u = 0;
@@ -19,27 +20,21 @@ struct vec2
 
 struct model
 {
-    int numVertices{0};
+    int numFaces{0};
     bool hasNormals{false};
-    bool hasTectureCoordinates{false};
+    bool hasTextureCoordinates{false};
     std::vector<vec3> vertices; // x, y, z (w=1.0)
     std::vector<vec3> normals; // x, y, z (w=1.0)
     std::vector<vec2> textureCoordinates; // u, v (w ignored)
-    std::vector<tri> tris;
+    std::vector<std::vector<int>> faces; // [[v1_idx, v2_idx, v3_idx], [...
+    // during loading this houses {vertix_idx, normal_idx, texture_idx}, ...
     // u_int32_t color = 0xffffffff;
 
     bool load(const std::string &path);
 private:
     bool handleLine(const std::string &line);
+    bool alignFaces();
 };
-
-// struct tri
-// {
-//     vec3 vertices[3];
-//     vec3 normals[3];
-//     vec3 textureCoordinates[3];
-//     u_int32_t color = 0xffffffff;
-// };
 
 // struct mesh
 // {
@@ -71,6 +66,9 @@ scene
 
 
 // adapted from: https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_olcEngine3D_Part3.cpp
+// load the mesh from an obj file
+// supports vertices, faces, normals
+// and texture coordinates
 bool model::load(const std::string &path)
 {
     std::ifstream f(path);
@@ -92,9 +90,62 @@ bool model::load(const std::string &path)
             return false;
         }
     }
+    if(!alignFaces())
+    {
+        std::cerr << "Trouble aligning faces" << std::endl;
+        return false;
+    }
+
     return true;
 }
 
+// utility to align vertices, normals
+// and texture coordinates for easy access
+bool model::alignFaces()
+{
+    // (deep-)copy the unsorted arrays
+    std::vector<vec3> _vertices = vertices;
+    std::vector<vec3> _normals = normals;
+    std::vector<vec2> _textureCoordinates = textureCoordinates;
+    std::vector<std::vector<int>> _faces = faces; // v1_idx, v2_idx, v3_idx, ...
+
+    // reset the model arrays
+    vertices.clear();
+    normals.clear();
+    textureCoordinates.clear();
+    faces.clear();
+
+    // populate the arrays back in order
+    // so that each have same id for same vertex
+    int i = 0;
+    for (auto vnt : _faces)
+    {
+        vertices.insert( vertices.end(),
+            { _vertices[vnt[0]], _vertices[vnt[3]], _vertices[vnt[6]]}
+        );
+
+        if (hasNormals)
+        {
+            normals.insert( normals.end(),
+                {_normals[vnt[1]], _normals[vnt[4]], _normals[vnt[7]]}
+            );
+        }
+
+        if (hasTextureCoordinates)
+        {
+            textureCoordinates.insert( textureCoordinates.end(),
+                { _textureCoordinates[vnt[2]], _textureCoordinates[vnt[5]], _textureCoordinates[vnt[8]]}
+            );
+        }
+        std::vector<int> face{ i, i+1, i+2 };
+        faces.insert( faces.end(), face);
+        i += 3;
+    }
+    numFaces = faces.size();
+    return true;
+}
+
+// utility to handle a obj file line
 bool model::handleLine(const std::string &line)
 {
     std::strstream s;
@@ -126,18 +177,17 @@ bool model::handleLine(const std::string &line)
     }
     else if (line.rfind("f ", 0) == 0) // face
     {
-        // std::cout << line << std::endl;
-
+        // e.g. f 1/11/111 2/22/222 3/33/333 (there are other types!)
         std::string vertexDetails;
-        int f[50]; // indices for triangle face...
+        int vrt[50]; // vertex indices
+        int nrm[50]; // normal indices
+        int tex[50]; // texture cordinate indices
         int i = 0;
 
         s >> junk;
 
         while (s >> vertexDetails)
         {
-            // std::cout << vertexDetails << std::endl;
-
             std::vector<int> indices;
             size_t last = 0;
             size_t next = 0;
@@ -150,20 +200,29 @@ bool model::handleLine(const std::string &line)
             indices.push_back(std::stoi(vertexDetails.substr(last)));
 
             // std::cout << "[";
-            // for (auto elem : indices) { std::cout << elem << ", "; };
-            // std::cout << "]" << std::endl;
+            // for (auto elem : indices) { std::cout << " " << elem; };
+            // std::cout << " ]" << std::endl;
 
             auto n = std::count(vertexDetails.begin(), vertexDetails.end(), '/');
             switch (n)
             {
             case 0: // just vertex indices: f 1 2 3
-                f[i] = indices.at(0);
+                vrt[i] = indices.at(0);
+                nrm[i] = 0; // ends up as -1
+                tex[i] = 0; // ends up as -1
                 break;
             case 1: // vertex and texture coordinates: f 1/7 2/8 3/9
-                f[i] = indices.at(0);
+                vrt[i] = indices.at(0);
+                tex[i] = indices.at(1);
+                nrm[i] = 0; // ends up as -1
+                hasTextureCoordinates = true;
                 break;
             case 2: // vertex, texture coordinates and normals: f 1/7/12 2/8/13 3/9/14
-                f[i] = indices.at(0);
+                vrt[i] = indices.at(0);
+                tex[i] = indices.at(1);
+                nrm[i] = indices.at(2);
+                hasNormals = true;
+                hasTextureCoordinates = true;
                 break;
             default:
                 std::cerr << "Trouble reading face vertex information: " << vertexDetails << std::endl;
@@ -171,8 +230,21 @@ bool model::handleLine(const std::string &line)
             }
             i++;
         }
-        u_int32_t color = 0xffffffff;
-        tris.push_back({{vertices[f[0] - 1], vertices[f[1] - 1], vertices[f[2] - 1]}, color});
+        if (i>3)
+        {
+            std::cerr << "Warning, clipping a polygon of " << i << " vertices !!!" << std::endl;
+        }
+
+        // TODO: for 4-vertex polygon, do the triangulation manually?
+
+        // pack the scattered indices to each face in v1, n1, t1, v2, n2, t2...
+        std::vector<int> face;
+        for (int i : {0, 1, 2})
+        {
+            face.insert( face.end(), { vrt[i] - 1, nrm[i] - 1, tex[i] -1} );
+        }
+        faces.push_back(face);
+
     }
     return true;
 }
--- test/modelImporter.test.cpp
+++ test/modelImporter.test.cpp
@@ -6,10 +6,13 @@ TEST_CASE("can import the Utah classic teapot")
     minity::model mdl{};
     bool res = mdl.load("test/models/teapot.obj");
     REQUIRE(res);
-    REQUIRE(mdl.vertices.size() == 3644);
+    REQUIRE(mdl.numFaces == 6320);
+    REQUIRE(!mdl.hasNormals);
+    REQUIRE(!mdl.hasTextureCoordinates);
+    REQUIRE(mdl.vertices.size() == 18960); // was 3644;
     REQUIRE(mdl.normals.size() == 0); // no normals in this primitive model
     REQUIRE(mdl.textureCoordinates.size() == 0); // no normals in this primitive model
-    REQUIRE(mdl.tris.size() == 6320);
+    REQUIRE(mdl.faces.size() == 6320);
 }
 
 TEST_CASE("can handle full face line")
@@ -17,8 +20,11 @@ TEST_CASE("can handle full face line")
     minity::model mdl{};
     bool res = mdl.load("test/models/MaleLow.obj");
     REQUIRE(res);
-    REQUIRE(mdl.vertices.size() == 810);
-    REQUIRE(mdl.normals.size() == 3752);
-    REQUIRE(mdl.textureCoordinates.size() == 1230);
-    REQUIRE(mdl.tris.size() == 1126);
+    REQUIRE(mdl.numFaces == 1126);
+    REQUIRE(mdl.hasNormals);
+    REQUIRE(mdl.hasTextureCoordinates);
+    REQUIRE(mdl.vertices.size() == 3378); // was 810
+    REQUIRE(mdl.normals.size() == 3378); // was 3752
+    REQUIRE(mdl.textureCoordinates.size() == 3378); // was 1230
+    REQUIRE(mdl.faces.size() == 1126);
 }
